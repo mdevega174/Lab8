ad37ddca9f35e79501b89e2a72126e0a9cd8c04f
mdevega174 <147475153+mdevega174@users.noreply.github.com>
2023-10-12 09:35:29 +0800

    Add files via upload

---
+++ CS201-MDEVEGA A#8/script.js
@@ -0,0 +1,13 @@
+const favSingers = ['Bruno Mars', 'Nicki Minaj', 'Taylor Swift'];
+console.log(favSingers);
+
+const favNumbers = ['2', '6', '22', '12'];
+console.log(favNumbers);
+
+const mixedArr = ["string", ["otherarray"], 123, true];
+
+console.log(favSingers[0]);
+console.log(favSingers[1]);
+console.log(favSingers[2]);
+console.log(favNumbers[1]);
+console.log(mixedArr[3]);
